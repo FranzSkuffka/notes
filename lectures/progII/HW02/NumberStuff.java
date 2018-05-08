2b23cfc9fbfb606c75a3d5c44da62a91230694c6
Jan Wirth <jan.wirth@mercedes-benz.io>
2018-05-08 14:24:33 +0200

    Complete numberstuff

--- lectures/progII/HW02/NumberStuff.java
+++ lectures/progII/HW02/NumberStuff.java
@@ -4,11 +4,11 @@ public class NumberStuff {
     int a[]=new int[100];//declaration and instantiation  
     for(int i=0;i <a.length;i++) {
       a[i]=i;//initialization
+      if(i % 3 == 0)
+        System.err.println(i);
+      if(i % 5 == 0)
+        System.err.println(i);
       sum+=i;
-    }
-    System.out.println(sum);
-    for(int i=0;i<a.length;i++) {
-      System.out.println(a[i]);
     }
   }
 }
