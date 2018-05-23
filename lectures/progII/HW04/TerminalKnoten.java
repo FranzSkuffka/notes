78fdc8a8e724ae8010c042bde852352c5c3d89a5
Jan Wirth <jottenwe@gmail.com>
2018-05-23 18:52:24 +0200

    Improve prog homework

--- lectures/progII/HW04/TerminalKnoten.java
+++ lectures/progII/HW04/TerminalKnoten.java
@@ -1,17 +1,21 @@
+// Models a terminal node in a parse tree
 public class TerminalKnoten extends Knoten {
 
     public String form;
+
     public String subType;
 
     public TerminalKnoten(String type, String form, String subType) {
         super(type);
+
         this.form = form;
+
         switch (type) {
             case "AFFIX":
                 if (subType == "D" || subType == "F") {
                     this.subType = subType;
                 } else {
-                    System.out.println("Your knot is of type " + type + ". Subtype can only be D or F! You gave " + subType + "!");
+                    System.out.println("Your node is of type " + type + ". Subtype must be either D or F. You gave " + subType + ".");
                 }
                 break;
             default:
@@ -21,17 +25,17 @@ public class TerminalKnoten extends Knoten {
     }
 
     public String getForm() {
-        // Returns form of terminal knot
+        // Return form
         return this.form;
     }
 
     public String getSubType() {
-        // Returns subtype of terminal knot
+        // Return subtype
         return this.subType;
     }
 
     public String toString() {
-        // Return string representation of instance including of what type, form and subtype it is
+        // String representation of instance: name, type, form and subtype
         return getClass().getName() + " " + type + ":" + form + ":" + subType;
     }
 }
