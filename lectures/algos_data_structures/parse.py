8065626d6c8da9ef3eccaf3b8943cdb8d377525e
Jan Wirth <jottenwe@gmail.com>
2018-06-01 09:56:06 +0200

    Complete HW05 IAD

--- lectures/algos_data_structures/parse.py
+++ lectures/algos_data_structures/parse.py
@@ -3,6 +3,26 @@ import re
 OPERATORS = '|\+|\-|\*|\/'
 NUMBERS = '\(?[0-9]\)?'
 
+OPERATOR_FNS = {
+  '+': lambda x, y: x + y,
+  '-': lambda x, y: x - y,
+  '*': lambda x, y: x * y,
+  '/': lambda x, y: x / y
+}
+
+class NodeNum:
+    def __init__(self, value):
+        v = int(value)
+        self.value = v
+    def eval(self):
+        return self.value
+class NodeOp:
+    def __init__(self, value):
+        self.value = value
+        self.left = self.right = None
+    def eval(self):
+        return OPERATOR_FNS[self.value](self.left.eval(), self.right.eval())
+
 def groupRest(restParts):
     opening = 0
     closing = 0
@@ -37,23 +57,42 @@ def split(s):
     return res
 
 def build(nested):
+  # flattwen double-nested list
+  if len(nested) == 1 and isinstance(nested, list) and isinstance(nested[0], list):
+    nested = nested[0]
+  rootOpPos = findRootOperatorPosition(nested)
+  # if an operator was found, recurse
+  if rootOpPos:
+    root = NodeOp(nested[rootOpPos])
+    root.left = build(nested[0:rootOpPos])
+    root.right = build(nested[rootOpPos + 1: len(nested)])
+    return root
+  else:
+    return NodeNum(nested[0])
+
+def findRootOperatorPosition(nested):
+  # we reverse the list because the leftmost operator has the highest precedence and we can't iterate from right to left
   rev = list(reversed(nested))
-  least = None
+  # we first want the lesser-binding leftmost operators (+, -)
+  # multiplication and division bind stronger, so we check their positions first and overwrite them with plus/minus positions if found
+  leftMostLeastBindingOperatorPosition = None
   for index, part in enumerate(rev):
     if not(isinstance(part, list)) and ('*' in part or '/' in part):
-      print(part)
-      least = index
+      leftMostLeastBindingOperatorPosition = index
   for index, part in enumerate(rev):
     if not isinstance(part, list) and ('+' in part or '-' in part):
-      print(part, type(part), not isinstance(part, list))
-      least = index
-  actual = len(nested) - least - 1
-  print(actual)
-  print(nested[0:actual], nested[actual], nested[actual + 1: len(nested)])
-  return (nested[0:actual], nested[actual], nested[actual + 1: len(nested)])
+      leftMostLeastBindingOperatorPosition = index
+  if leftMostLeastBindingOperatorPosition:
+    # reverse index back
+    return len(nested) - leftMostLeastBindingOperatorPosition - 1
+  else:
+    return None
 
 def parse(s):
+  print('parsing', s)
   nested = split(s)
-  return build(nested)
+  tree = build(nested)
+  return tree
 
-print(parse('2*4*(3+(4-7)*8)-(1-6)'))
+res = parse('2*4*(3+(4-7)*8)-(1-6)')
+print(res, res.eval())
